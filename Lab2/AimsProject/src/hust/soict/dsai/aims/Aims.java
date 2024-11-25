f5cce94db313879401ff1b11a7ce29881e186d9d
Lngngyn289 <longnguyenduc98921@gmail.com>
2024-11-25 23:42:46 +0700

    lab3

--- Lab2/AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ Lab2/AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,7 +1,9 @@
-package AimsPackage;
+
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
 public class Aims {
-
 	public static void main(String[] args) {
 		Cart anOrder = new Cart();
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
