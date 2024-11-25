da018010f96771f28dafe7d5f993da5da3a1f76a
Lngngyn289 <longnguyenduc98921@gmail.com>
2024-11-26 01:09:51 +0700

    sua lab3

--- Lab2/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ Lab2/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -1,10 +1,22 @@
 package hust.soict.dsai.garbage;
-
+import java.util.Random;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
 public class ConcatenationInLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Random r = new Random(123);
+         long start = currentTimeMillis();
+         String s = "";
+         for (int i= 0;i<65536;i++) s+=r.nextInt(2);
+         System.out.println(currentTimeMillis() - start);
 
+         r = new Random(123);
+         start = System.currentTimeMillis();
+         StringBuilder sb = new StringBuilder();
+         for (int i= 0;i<65536;i++) sb.append(r.nextInt(2));
+         s+= sb.toString();
+         System.out.println(currentTimeMillis() - start);
 	}
 
 }
